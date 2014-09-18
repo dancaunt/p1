72f59afe16014e76440d4a83f916ef58fa9fcee0
Dan <dcaunt@fas.harvard.edu>
2014-09-17 23:59:44 -0400

    Updated index.php with personal info.  Added /images and a picture of myself.

--- index.php
+++ index.php
@@ -2,7 +2,26 @@
 <head>
 </head>
 <body>
-P1
-First Edit
+<h1>Daniel Caunt<br>Dynamic Web Apps Portfolio</h1>
+<img src='/images/1d2abae.jpg' alt='Dan'>
+<h3>Project 2</h3>
+View: [link]
+Github: [link]
+<h3>Project 3</h3>
+View: [link]
+Github: [link]
+<h3>Project 4</h3>
+View: [link]
+Github: [link]
+<h2>In 2+ sentences describe who you are and what you do.</h2>
+<p>I'm the systems administrator for the Harvard Language Resource Center.  I am also an amateur astronomer and avid hockey fan.</p>
+<h2>Why are you taking this course?</h2>
+<p>I would like to refresh my web application programming skills since I haven't done any web development in a while.</p>
+<h2>What previous experience do you have with programming?</h2>
+<p>I have been a student of programming for the last two decades.  My first programming class was in GW Basic in 1995.  I have taken courses in C, C++, Java, as well as mobile app development.  However, I don't use these skills on a day-to-day basis so I need to refresh them periodically.</p>
+<h2>Are you working on a Mac, PC, or some other platform?</h2>
+<p>I am developing on a Mac.</p>
+<h2>Anything else we should know?</h2>
+<p>I am hosting my website at home instead of using Digital Ocean.  My hosting platform is SuSE Linux on a Dell PC running a KVM virtual CentOS server.</p>
 </body>
 </html>
