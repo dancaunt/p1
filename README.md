006f5efb3d91e698283229709659235e9c183b90
Dan <dcaunt@fas.harvard.edu>
2014-09-19 10:56:26 -0400

    Added CSS and put live URL in README

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ p1
 ==
 
 Project 1
+
+The live URL for Project 1 is:
+p1.rabbithill.org
--- index.php
+++ index.php
@@ -1,27 +1,63 @@
 <html>
 <head>
+
+<title>Daniel Caunt P1 - DWA15 Portfolio</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel='stylesheet' href='bootstrap.min.css' type='text/css'>
+<link href="bootstrapflatly.min.css" rel="stylesheet">
+
+<style>
+ h1 {
+  text-align:center;
+ }
+ .container {
+	width:50%;
+	min-width:600px;
+	margin-bottom:50px;
+ }	
+ img {
+	display:block;
+	margin:auto;
+	width:33%;
+ }
+</style>
+
 </head>
 <body>
+
+<div class="container">
 <h1>Daniel Caunt<br>Dynamic Web Apps Portfolio</h1>
 <img src='images/1d2abae.jpg' alt='Dan'>
+
 <h3>Project 2</h3>
 View: [link]
 Github: [link]
+
 <h3>Project 3</h3>
 View: [link]
 Github: [link]
+
 <h3>Project 4</h3>
 View: [link]
 Github: [link]
+
 <h2>In 2+ sentences describe who you are and what you do.</h2>
 <p>I'm the systems administrator for the Harvard Language Resource Center.  I am also an amateur astronomer and avid hockey fan.</p>
+
 <h2>Why are you taking this course?</h2>
 <p>I would like to refresh my web application programming skills since I haven't done any web development in a while.</p>
+
 <h2>What previous experience do you have with programming?</h2>
 <p>I have been a student of programming for the last two decades.  My first programming class was in GW Basic in 1995.  I have taken courses in C, C++, Java, as well as mobile app development.  However, I don't use these skills on a day-to-day basis so I need to refresh them periodically.</p>
+
 <h2>Are you working on a Mac, PC, or some other platform?</h2>
 <p>I am developing on a Mac.</p>
+
 <h2>Anything else we should know?</h2>
 <p>I am hosting my website at home instead of using Digital Ocean.  My hosting platform is SuSE Linux on a Dell PC running a KVM virtual CentOS server.</p>
+
+</div>
 </body>
 </html>
